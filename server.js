06c6f2952b9b44ac25aec28e158962cae84d2d4e
Nirmal Raj <30683017+NirmalRaj354@users.noreply.github.com>
2017-08-08 21:29:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/article-one',function(req, res) {
+  res.send('Ariticle one is requsted and will be serverd here'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
