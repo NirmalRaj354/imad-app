0f8de8d80b3ee5845051b8574f2f6fc64daf6131
Nirmal Raj <30683017+NirmalRaj354@users.noreply.github.com>
2017-08-08 21:34:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one', function(req, res) {
   res.send('ariticle one is requsted and will be serverd here');
 });
 
+app.get('/article-two', function(req, res) {
+  res.send('ariticle Two is requsted and will be serverd here');
+});
+
+app.get('/article-three', function(req, res) {
+  res.send('ariticle Three is requsted and will be serverd here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
