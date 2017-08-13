abf51d4d2b22c534ec994a396eaa5db5813ca691
Nirmal Raj <30683017+NirmalRaj354@users.noreply.github.com>
2017-08-13 21:01:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //move the image
-var img = document.getElementById('madi');
+var img = document.getElementById('img');
 img.oncliclk = function() {
  img.style.marginLeft = '100px';
  };
