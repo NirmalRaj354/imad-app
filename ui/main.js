3c1cdea34a376dcc0fd8c6ec9c8606d3896652b3
Nirmal Raj <30683017+NirmalRaj354@users.noreply.github.com>
2017-08-14 10:22:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,4 +22,21 @@ button.onclick = function() {
     //Make the request
         request.open('GET','http://rajnirmal354.imad.hasura-app.io/counter',true);
         request.send(null);
-}; 
+}; //Submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+
+ //Make the request to the server and send the name
+ //capture a list of name and render it as a list.
+ 
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for (var i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
