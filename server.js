dd567c99a0a5339609d1d05c25b2353e9f4954ca
Nirmal Raj <30683017+NirmalRaj354@users.noreply.github.com>
2017-08-23 22:14:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     user:'rajnirmal354',
     database:'rajnirmal354',
     host: 'http://db.imad.hasura-app.io',
-    port:'5432',
+    port:'8080',
     password: process.env.DB_PASSWORD
     
 };
